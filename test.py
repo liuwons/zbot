d4f691b00402c17bc4bf6aa9cb9c30543bbaa40a
liuwons <liuwons@163.com>
2018-02-26 21:17:48 +0800

    [func] zb api

--- test.py
+++ test.py
@@ -1,10 +1,41 @@
+import json
 import zapi
 
 
 def main():
-    zpi = zapi.ZApi()
-    markets = zpi.markets()
-    print markets
+    conf = json.load(open('conf.json'))
+    if 'zb' not in conf:
+        print 'configure file error, read zb failed'
+        return
+    if 'access_key' not in conf['zb']:
+        print 'configure file error, read access_key failed'
+        return
+    if 'secret_key' not in conf['zb']:
+        print 'configure file error, read secret_key failed'
+        return
+    access_key = conf['zb']['access_key']
+    secret_key = conf['zb']['secret_key']
+
+    zpi = zapi.ZApi(str(access_key), str(secret_key))
+
+    # print zpi.markets()
+    # print zpi.ticker("btc_qc")
+    # print zpi.depth("btc_qc", 2)
+    # print zpi.trades("btc_qc")
+    # print zpi.kline("btc_qc")
+
+    # print zpi.order('zb_qc', zapi.ORDER_TYPE_BUY, '100', '5.0008')
+    # print zpi.cancel_order('zb_qc', '2018022615327073')
+    # print zpi.get_order('zb_qc', '2018022615328784')
+    # print zpi.get_orders('zb_qc', '1', zapi.ORDER_TYPE_BUY)
+    # print zpi.get_orders_new('zb_qc', '1', '20', zapi.ORDER_TYPE_BUY)
+    # print zpi.get_orders_ignore_tader_type('zb_qc', '1', '10')
+    # print zpi.get_unfinished_orders_ignore_trade_type('zb_qc', '1', '10')
+    # print zpi.get_account_info()
+    # print zpi.get_user_address('ltc')
+    # print zpi.get_withdraw_address('ltc')
+    # print zpi.get_withdraw_record('ltc', '1', '10')
+    # print zpi.get_charge_record('ltc', '1', '10')
 
 if __name__ == '__main__':
     main()
--- zapi.py
+++ zapi.py
@@ -1,3 +1,7 @@
+import hashlib
+import struct
+import sha
+import time
 import requests
 
 
@@ -31,10 +35,51 @@ errcode = {
     "4002": "requests too frequently"
 }
 
+ORDER_TYPE_BUY = '1'
+ORDER_TYPE_SELL = '0'
+
+TPL_MARKETS = 'http://api.zb.com/data/v1/markets'
+TPL_MARKET = 'http://api.zb.com/data/v1/ticker?market={MARKET}'
+TPL_DEPTH = 'http://api.zb.com/data/v1/ticker?market={MARKET}&size={SIZE}'
+TPL_TRADES = 'http://api.zb.com/data/v1/trades?market={MARKET}'
+TPL_KLINE = 'http://api.zb.com/data/v1/kline?market={MARKET}'
+
+URL_ORDER = 'https://trade.zb.com/api/order'
+URL_CANCEL_ORDER = 'https://trade.zb.com/api/cancelOrder'
+URL_GET_ORDER = 'https://trade.zb.com/api/getOrder'
+URL_GET_ORDERS = 'https://trade.zb.com/api/getOrders'
+URL_GET_ORDERS_NEW = 'https://trade.zb.com/api/getOrdersNew'
+URL_GET_ORDERS_IGNORE_TRADE_TYPE = 'https://trade.zb.com/api/getOrdersIgnoreTradeType'
+URL_GET_UNFINISHED_ORDERS_IGNORE_TRADE_TYPE = 'https://trade.zb.com/api/getUnfinishedOrdersIgnoreTradeType'
+URL_GET_ACCOUNT_INFO = 'https://trade.zb.com/api/getAccountInfo'
+URL_GET_USER_ADDRESS = 'https://trade.zb.com/api/getUserAddress'
+URL_GET_WITHDRAW_ADDRESS = 'https://trade.zb.com/api/getWithdrawAddress'
+URL_GET_WITHDRAW_RECORD = 'https://trade.zb.com/api/getWithdrawRecord'
+URL_GET_CHARGE_RECORD = 'https://trade.zb.com/api/getChargeRecord'
+URL_WITHDRAW = 'https://trade.zb.com/api/withdraw'
+
 
 class ZApi:
-    def __init__(self):
-        pass
+    def __init__(self, access_key, secret_key):
+        """
+        opened markets and status, format:
+            {
+                "btc_usdt": {
+                    "amountScale": 4,
+                    "priceScale": 2
+                },
+                "ltc_usdt": {
+                    "amountScale": 3,
+                    "priceScale": 2
+                }
+                ...
+            }
+        """
+        self._access_key_ = access_key
+        self._secret_key_ = secret_key
+        self._markets_ = self.markets()
+        if len(self._markets_) < 1:
+            raise Exception("Get markets status failed")
 
     def get(self, url):
         r = requests.get(url)
@@ -42,63 +87,452 @@ class ZApi:
             raise Exception("Request error happens, url: " + url + "  code: " + str(r.status_code))
         return r.json()
 
+    def check_market_code(self, market):
+        if not market:
+            return False
+        if market not in self._markets_:
+            return False
+        else:
+            return True
+
     def markets(self):
-        url = 'http://api.zb.com/data/v1/markets'
-        result = ""
-        try:
-            result = self.get(url)
-        except Exception, arg:
-            print arg
-        return result
+        """
+        markets data, result format:
+            {
+                "btc_usdt": {
+                    "amountScale": 4,
+                    "priceScale": 2
+                },
+                "ltc_usdt": {
+                    "amountScale": 3,
+                    "priceScale": 2
+                }
+                ...
+            }
+        :return: result data of markets
+        """
+        url = TPL_MARKETS
+        return self.get(url)
 
-    def ticker(self):
-        pass
+    def ticker(self, market):
+        """
+        ticker data, result format:
+            {
+                "ticker": {
+                    "vol": "40.463",
+                    "last": "0.899999",
+                    "sell": "0.5",
+                    "buy": "0.225",
+                    "high": "0.899999",
+                    "low": "0.081"
+                },
+                "date": "1507875747359"
+            }
+        :param market: market code string
+        :return: ticker result
+        """
+        url = TPL_MARKET.format(MARKET=market)
+        return self.get(url)
 
-    def depth(self):
-        pass
+    def depth(self, market, size):
+        """
+        depth data of specific market, result format:
+            {
+                "asks": [
+                    [
+                        83.28,
+                        11.8
+                    ]...
+                ],
+                "bids": [
+                    [
+                        81.91,
+                        3.65
+                    ]...
+                ],
+                "timestamp" : timestamp
+            }
+        :param market: market code string
+        :param size: depth size
+        :return: result depth data
+        """
+        url = TPL_DEPTH.format(MARKET=market, SIZE=str(size))
+        return self.get(url)
 
-    def trades(self):
-        pass
+    def trades(self, market):
+        """
+        trade history, result format:
+            [
+                {
+                    "amount": 0.541,
+                    "date": 1472711925,
+                    "price": 81.87,
+                    "tid": 16497097,
+                    "trade_type": "ask",
+                    "type": "sell"
+                }...
+            ]
+        :return: trade history data
+        """
+        url = TPL_TRADES.format(MARKET=market)
+        return self.get(url)
 
-    def kline(self):
-        pass
+    def kline(self, market):
+        """
+        kline data, result format:
+            {
+                "data": [
+                    [
+                        1472107500000,
+                        3840.46,
+                        3843.56,
+                        3839.58,
+                        3843.3,
+                        492.456
+                    ]...
+                ],
+                "moneyType": "btc",
+                "symbol": "ltc"
+            }
+        :param market: market code
+        :return: kline data
+        """
+        url = TPL_KLINE.format(MARKET=market)
+        return self.get(url)
 
-    def order(self):
-        pass
+    def order(self, market, type, amount, price):
+        """
+        order api, result format:
+            {
+                "code": "1000",
+                "message": "***",
+                "id": "20131228361867"
+            }
+        :param market: currency
+        :param type: ORDER_TYPE_BUY or ORDER_TYPE_SELL
+        :param amount: order amount
+        :param price: order price
+        :return: order result
+        """
+        params = 'accesskey=%s&amount=%s&currency=%s&method=order&price=%s&tradeType=%s' % (self._access_key_, amount, market, price, type)
+        return self.call_api(url=URL_ORDER, params=params)
 
-    def cancel_order(self):
-        pass
+    def cancel_order(self, market, order_id):
+        """
+        cancel an order, result format:
+            {
+                "code": "1000",
+                "message": "***"
+            }
+        :param market: currency
+        :param order_id: order id
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&id=%s&method=cancelOrder' % (self._access_key_, market, order_id)
+        return self.call_api(url=URL_CANCEL_ORDER, params=params)
 
-    def get_order(self):
-        pass
+    def get_order(self, market, order_id):
+        """
+        get order detail, result format:
+            {
+                "currency": "btc",
+                "id": "20150928158614292",
+                "price": 1560,
+                "status": 3,
+                "total_amount": 0.1,
+                "trade_amount": 0,
+                "trade_price" : 6000,
+                "trade_date": 1443410396717,
+                "trade_money": 0,
+                "type": 0,
+            }
+        :param market: currency
+        :param order_id: order id
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&id=%s&method=getOrder' % (self._access_key_, market, order_id)
+        return self.call_api(url=URL_GET_ORDER, params=params)
 
-    def get_orders(self):
-        pass
+    def get_orders(self, market, page, type):
+        """
+        get multiple orders, result format:
+            [
+                {
+                    "currency": "btc",
+                    "id": "20150928158614292",
+                    "price": 1560,
+                    "status": 3,
+                    "total_amount": 0.1,
+                    "trade_amount": 0,
+                    "trade_price" : 6000,
+                    "trade_date": 1443410396717,
+                    "trade_money": 0,
+                    "type": 0
+                }...
+            ]
+        :param market: currency
+        :param page: page index
+        :param type: order type
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&method=getOrders&pageIndex=%s&tradeType=%s' % (self._access_key_, market, page, type)
+        return self.call_api(url=URL_GET_ORDERS, params=params)
 
-    def get_orders_new(self):
-        pass
+    def get_orders_new(self, market, page_index, page_size, type):
+        """
+        get multiple orders, result format same as get_orders
+        :param market: currency
+        :param page_index: page index
+        :param page_size: page size
+        :param type: order type
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&method=getOrdersNew&pageIndex=%s&pageSize=%s&tradeType=%s' % (self._access_key_, market, page_index, page_size, type)
+        return self.call_api(url=URL_GET_ORDERS_NEW, params=params)
 
-    def get_orders_ignore_tader_type(self):
-        pass
+    def get_orders_ignore_tader_type(self, market, page_index, page_size):
+        """
+        get multiple orders ignore type, result format same as get_orders
+        :param market: currency
+        :param page_index: page index
+        :param page_size: page size
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&method=getOrdersIgnoreTradeType&pageIndex=%s&pageSize=%s' % (self._access_key_, market, page_index, page_size)
+        return self.call_api(url=URL_GET_ORDERS_IGNORE_TRADE_TYPE, params=params)
 
-    def get_unfinished_orders_ignore_trade_type(self):
-        pass
+    def get_unfinished_orders_ignore_trade_type(self, market, page_index, page_size):
+        """
+        get multiple unfinished orders ignore type, result format same as get_orders
+        :param market: currency
+        :param page_index: page index
+        :param page_size: page size
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&method=getUnfinishedOrdersIgnoreTradeType&pageIndex=%s&pageSize=%s' % (self._access_key_, market, page_index, page_size)
+        return self.call_api(url=URL_GET_UNFINISHED_ORDERS_IGNORE_TRADE_TYPE, params=params)
 
     def get_account_info(self):
-        pass
+        """
+        get account information, result format:
+            {
+                "result": {
+                    "coins": [
+                        {
+                            "freez": "0.00000000",
+                            "enName": "BTC",
+                            "unitDecimal": 8,
+                            "cnName": "BTC",
+                            "unitTag": "*",
+                            "available": "0.00000000",
+                            "key": "btc"
+                        },
+                        {
+                            "freez": "0.00000000",
+                            "enName": "LTC",
+                            "unitDecimal": 8,
+                            "cnName": "LTC",
+                            "unitTag": "*",
+                            "available": "0.00000000",
+                            "key": "ltc"
+                        },
+                       ...
+                    ],
+                    "base": {
+                        "username": "134150***",
+                        "trade_password_enabled": true,
+                        "auth_google_enabled": false,
+                        "auth_mobile_enabled": true
+                    }
+                }
+            }
+        :return: result
+        """
+        params = 'accesskey=%s&method=getAccountInfo' % self._access_key_
+        return self.call_api(url=URL_GET_ACCOUNT_INFO, params=params)
 
-    def get_user_address(self):
-        pass
+    def get_user_address(self, currency):
+        """
+        get user address, result format:
+            {
+                "code": 1000,
+                "message": {
+                    "des": "success",
+                    "isSuc": true,
+                    "datas": {
+                        "key": "0x0af7f36b8f09410f3df62c81e5846da673d4d9a9"
+                    }
+                }
+            }
+        :param currency: currency
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&method=getUserAddress' % (self._access_key_, currency)
+        return self.call_api(url=URL_GET_USER_ADDRESS, params=params)
 
-    def get_withdraw_address(self):
-        pass
+    def get_withdraw_address(self, currency):
+        """
+        get withdraw address, result format:
+            {
+                "code": 1000,
+                "message": {
+                    "des": "success",
+                    "isSuc": true,
+                    "datas": {
+                        "key": "0x0af7f36b8f09410f3df62c81e5846da673d4d9a9"
+                    }
+                }
+            }
+        :param currency: currency
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&method=getWithdrawAddress' % (self._access_key_, currency)
+        return self.call_api(url=URL_GET_WITHDRAW_ADDRESS, params=params)
 
-    def get_withdraw_record(self):
-        pass
+    def get_withdraw_record(self, currency, page_index, page_size):
+        """
+        get withdraw record, result format:
+            {
+                "code": 1000,
+                "message": {
+                    "des": "success",
+                    "isSuc": true,
+                    "datas": {
+                        "list": [
+                            {
+                                "amount": 0.01,
+                                "fees": 0.001,
+                                "id": 2016042556231,
+                                "manageTime": 1461579340000,
+                                "status": 3,
+                                "submitTime": 1461579288000,
+                                "toAddress": "14fxEPirL9fyfw1i9EF439Pq6gQ5xijUmp"
+                            }...
+                        ],
+                        "pageIndex": 1,
+                        "pageSize": 10,
+                        "totalCount": 4,
+                        "totalPage": 1
+                    }
+                }
+            }
+        :param currency:
+        :param page_index:
+        :param page_size:
+        :return:
+        """
+        params = 'accesskey=%s&currency=%s&method=getWithdrawRecord&pageIndex=%s&pageSize=%s' % (self._access_key_, currency, page_index, page_size)
+        return self.call_api(url=URL_GET_WITHDRAW_RECORD, params=params)
 
-    def get_charge_record(self):
-        pass
+    def get_charge_record(self, currency, page_index, page_size):
+        """
+        get charge record, result format:
+            {
+                "code": 1000,
+                "message": {
+                    "des": "success",
+                    "isSuc": true,
+                    "datas": {
+                        "list": [
+                            {
+                                "address": "1FKN1DZqCm8HaTujDioRL2Aezdh7Qj7xxx",
+                                "amount": "1.00000000",
+                                "confirmTimes": 1,
+                                "currency": "BTC",
+                                "description": "***",
+                                "hash": "7ce842de187c379abafadd64a5fe66c5c61c8a21fb04edff9532234a1dae6xxx",
+                                "id": 558,
+                                "itransfer": 1,
+                                "status": 2,
+                                "submit_time": "2016-12-07 18:51:57"
+                            }...
+                        ],
+                        "pageIndex": 1,
+                        "pageSize": 10,
+                        "total": 8
+                    }
+                }
+            }
+        :param currency: currency
+        :param page_index: page index
+        :param page_size: page size
+        :return: result
+        """
+        params = 'accesskey=%s&currency=%s&method=getChargeRecord&pageIndex=%s&pageSize=%s' % (self._access_key_, currency, page_index, page_size)
+        return self.call_api(url=URL_GET_CHARGE_RECORD, params=params)
 
-    def withdraw(self):
-        pass
+    def withdraw(self, currency, amount, fees, itransfer, addr, pwd):
+        """
+        withdraw coins, result format:
+            {
+                "code": 1000,
+                "message": "success",
+                "id": "***"
+            }
+        :param currency: currency
+        :param amount: amount
+        :param fees: fees
+        :param itransfer: itransfer
+        :param addr: receive address
+        :param pwd: safe password of current account
+        :return: result
+        """
+        params = 'accesskey=%s&amount=%s&currency=%s&fees=%s&itransfer=%s&method=withdraw&receiveAddr=%s&safePwd=%s' % (self._access_key, amount, currency, fees, itransfer, addr, pwd)
+        return self.call_api(url=URL_WITHDRAW, params=params)
+
+    def call_api(self, url, params=''):
+        full_url = url
+        if params:
+            sha_secret = self.digest(self._secret_key_)
+            sign = self.hmac_sign(params, sha_secret)
+            req_time = int(round(time.time() * 1000))
+            params += '&sign=%s&reqTime=%d' % (sign, req_time)
+            full_url += '?' + params
+        print 'call api: ', full_url
+        r = requests.get(full_url)
+        if r.status_code != 200:
+            raise Exception("Request error happens, url: " + url + "  code: " + str(r.status_code))
+        return r.json()
+
+    @staticmethod
+    def fill(value, lenght, fill_byte):
+        if len(value) >= lenght:
+            return value
+        else:
+            fill_size = lenght - len(value)
+        return value + chr(fill_byte) * fill_size
+
+    @staticmethod
+    def xor(s, value):
+        slist = list(s)
+        for index in xrange(len(slist)):
+            slist[index] = chr(ord(slist[index]) ^ value)
+        return "".join(slist)
+
+    def hmac_sign(self, arg_value, arg_key):
+        keyb = struct.pack("%ds" % len(arg_key), arg_key)
+        value = struct.pack("%ds" % len(arg_value), arg_value)
+        k_ipad = self.xor(keyb, 0x36)
+        k_opad = self.xor(keyb, 0x5c)
+        k_ipad = self.fill(k_ipad, 64, 54)
+        k_opad = self.fill(k_opad, 64, 92)
+        m = hashlib.md5()
+        m.update(k_ipad)
+        m.update(value)
+        dg = m.digest()
+
+        m = hashlib.md5()
+        m.update(k_opad)
+        subStr = dg[0:16]
+        m.update(subStr)
+        dg = m.hexdigest()
+        return dg
+
+    def digest(self, arg_value):
+        value = struct.pack("%ds" % len(arg_value), arg_value)
+        h = sha.new()
+        h.update(value)
+        dg = h.hexdigest()
+        return dg
+
 
